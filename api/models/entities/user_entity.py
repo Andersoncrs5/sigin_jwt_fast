6f07ab19de7930ec87658ba598f6d2992e531813
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-13 16:02:13 -0300

    create entity to user

---
+++ api/models/entities/user_entity.py
@@ -0,0 +1,18 @@
+from api.configs.db.database import Base
+from sqlalchemy.orm import declarative_base, Mapped, mapped_column
+from sqlalchemy import String
+from sqlalchemy import String, DateTime, func
+from datetime import datetime
+
+Base = declarative_base()
+
+class UserEntity(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(150), nullable=False, unique=True)
+    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    refresh_token: Mapped[str] = mapped_column(String(), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)

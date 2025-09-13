bedaea6b4db39852a008b3e0e649e5e164b563c4
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-13 20:22:25 -0300

    set route of auth controller

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 # python -m venv venv
 # source venv/bin/activate
 # pip install -r requirements.txt
+# uvicorn main:app --reload
 
 import logging
+from fastapi import FastAPI
+from api.controllers import auth_controller
+from api.configs.db.database import create_tables
 
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s"
 )
 logger = logging.getLogger(__name__)
+
+app = FastAPI()
+
+@app.on_event("startup")
+def start_up():
+    create_tables()
+
+app.include_router(auth_controller.app_router)

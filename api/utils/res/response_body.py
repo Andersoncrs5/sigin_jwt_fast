d3dca186ebc0ebb9278230883681fb46b040e444
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-13 20:26:08 -0300

    set responseBody to http response default

---
+++ api/utils/res/response_body.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+from typing import TypeVar, Generic
+
+T = TypeVar('T')
+
+class ResponseBody(BaseModel, Generic[T]):
+    code: int
+    message: str
+    body: T
+    status: bool

4fd7b2e78724be2eb54f4f3bdc76ce9ff2fe516f
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-13 20:24:50 -0300

    set to_user_entity to CreateUserDTO new class LoginDTO and  UpdateUserDTO

--- api/models/schemas/user_schema.py
+++ api/models/schemas/user_schema.py
@@ -1,10 +1,23 @@
 from pydantic import BaseModel
+from api.models.entities.user_entity import UserEntity
 
 class CreateUserDTO(BaseModel):
     name: str
     email: str
     password: str
 
+    def to_user_entity(self) -> UserEntity:
+        return UserEntity(
+            name= self.name,
+            email= self.email,
+            password= self.password,
+        )
+    
+
 class UpdateUserDTO(BaseModel):
     name: str | None
     password: str | None
+
+class LoginDTO(BaseModel):
+    email: str
+    password: str

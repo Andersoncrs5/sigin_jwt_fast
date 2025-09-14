f2424b0a110539b48da9d8049345edbfee04b07a
Andersoncrs5 <anderson.c.rms2005@gmail.com>
2025-09-14 13:33:56 -0300

    new method to_user_out

--- api/models/entities/user_entity.py
+++ api/models/entities/user_entity.py
@@ -1,10 +1,8 @@
 from api.configs.db.database import Base
-from sqlalchemy.orm import declarative_base, Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String
 from sqlalchemy import String, DateTime, func
 from datetime import datetime
-
-# Base = declarative_base()
 
 class UserEntity(Base):
     __tablename__ = "users"
@@ -16,3 +14,12 @@ class UserEntity(Base):
     refresh_token: Mapped[str | None] = mapped_column(String(), nullable=True)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), onupdate=func.now(), nullable=False)
+
+    def to_user_out(self):
+        from api.models.schemas.user_schema import UserOUT
+
+        return UserOUT(
+            id = self.id,
+            name = self.name,
+            email = self.email
+        )
